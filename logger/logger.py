e911414163b43387b1f51faa9fa22238daa8ee1b
Erik Sjöberg <sjoerik@gmail.com>
2023-09-15 16:15:43 +0200

    Adding topic to logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,18 +1,19 @@
 import paho.mqtt.client as mqtt
 import os
 
-MQTT_BROKER = os.environ.get("MQTT_BROKER", "localhost")
+MQTT_BROKER = os.environ.get("MQTT_BROKER", "mosquitto")
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe("#")
+# Callback when a message is received
+def on_message(client, userdata, message):
+    print(f"Received message '{message.payload}' on topic '{message.topic}'")
 
-def on_message(client, userdata, msg):
-    print(f"{msg.topic} {str(msg.payload)}")
-
+# Setup MQTT client
 client = mqtt.Client()
-client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect(MQTT_BROKER, 1883, 60)
+
+# Subscribe to the desired topic
+client.subscribe("home/livingroom/temperature")
+
+# Start the client loop
 client.loop_forever()
